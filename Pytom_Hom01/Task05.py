a776c77d5e63f281eccf32408368b99835ed45d8
Pavel <black_yaguar@mail.ru>
2023-01-30 17:47:45 +0500

    Создание файла для пятой задачи

---
+++ Pytom_Hom01/Task05.py
@@ -0,0 +1,11 @@
+# ; Задача 2: - HARD необязательная, идет за 3 обязательных.
+# ; Найдите сумму цифр любого вещественного или целого числа.
+# ;  Можно использовать decimal. Через строку решать нельзя.
+from decimal import Decimal
+d=Decimal(input('fdkhkkkjh='))
+print(d)
+didital=Decimal('d').compare_total(Decimal('d'))
+#d=Decimal(getcontext(d))
+#didital=Decimal(Decimal(d).as_tuple().exponent*(-1))
+print(didital)
+print (Decimal(str(0.25456466424685)).as_tuple().exponent*(-1))
