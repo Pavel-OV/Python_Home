3c050bef84ea90c12c89478d422e771a3c9cc394
Pavel <black_yaguar@mail.ru>
2023-01-31 01:36:38 +0500

    Пятая задача домашней работы

--- Pytom_Hom01/Task05.py
+++ Pytom_Hom01/Task05.py
@@ -2,10 +2,34 @@
 # ; Найдите сумму цифр любого вещественного или целого числа.
 # ;  Можно использовать decimal. Через строку решать нельзя.
 from decimal import Decimal
-d=Decimal(input('fdkhkkkjh='))
-print(d)
-didital=Decimal('d').compare_total(Decimal('d'))
-#d=Decimal(getcontext(d))
+number=Decimal(input('Введите вещественное или целогое число ='))
+while number != int(number):
+    number=number* 10
+number=int(number)
+print(f'Теперь подсчитаем сумму цифр этого числа',number)
+digit=0
+while number>9:
+    count=number%10
+    digit=digit+count
+    number=number//10
+else:
+    digitl=digit+number
+print(digitl)
+
+
 #didital=Decimal(Decimal(d).as_tuple().exponent*(-1))
-print(didital)
-print (Decimal(str(0.25456466424685)).as_tuple().exponent*(-1))
+# print(didital)
+# print (Decimal(str(0.25456466424685)).as_tuple().exponent*(-1))
+
+
+
+# foo = a % 10
+# if foo != 0:
+#     # Then foo contains decimals
+#     pass
+
+# if foo == 0:
+#     # Then foo does NOT contain decimals Тогда foo не содержит десятичных знаков
+#     print('ghjgjjj')
+
+#     y=True if y>50 else False
