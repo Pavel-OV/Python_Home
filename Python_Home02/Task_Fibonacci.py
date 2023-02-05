434fa65e2086838d5f83d6a5172c688c303f836e
Pavel <black_yaguar@mail.ru>
2023-02-06 00:46:54 +0500

    Оформление дополнительной задачи

--- Python_Home02/Task_Fibonacci.py
+++ Python_Home02/Task_Fibonacci.py
@@ -11,6 +11,22 @@ fibonacci=int(input('индекс='))
 sum=0
 a=0
 b=1
-for i in range(fibonacci):
-    sum=a,a=b
-    b=b+a
+for i in range(fibonacci+1): # 
+    sum=a
+    a=b
+    b=a+sum
+    sp.append(sum)
+sp2=[]
+sum=0
+a=0
+b=1
+for i in range(fibonacci): 
+    sum=a
+    a=b
+    b=(a-sum)*(-1)
+    sp2.insert((0),a)
+sp3=[]
+sp3=sp2+sp
+print("Число Фибоначчи с отрицательныит числами \n", sp3)    
+
+
