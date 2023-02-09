5d6652fa8fdcc054a118a100b96e19cefe7f5ef7
Pavel <black_yaguar@mail.ru>
2023-02-09 15:14:03 +0500

    Оформление 20 задачи и попытка решения дополнительной задачи

--- Python_Home03/Hard_Task.py
+++ Python_Home03/Hard_Task.py
@@ -10,5 +10,51 @@
 # [1, 5, 2, 3, 4, 1, 7, 8 , 15 , 1 ] => [1, 5]
 
 # [1, 5, 3, 4, 1, 7, 8 , 15 , 1 ] => [3, 5]
-spisok=[1, 5, 2, 3, 4, 1, 7, 8 , 15 , 1]
+spisok=[1, 5, 3, 4, 1, 7, 8 , 15 , 1 ]
 print(spisok)
+min=max=midl=0
+min=spisok[0]
+max=spisok[1]
+if max<min:
+     max=spisok[0]
+     min=spisok[1]
+for i in range (len(spisok)):
+     if min>spisok[i]: min=spisok[i]
+     if max<spisok[i]: max=spisok[i]
+print(f"Итервал от{min} до {max}")
+count=0
+intervalmin=min
+intervalmax=max
+for min in range(len(spisok)):
+     for i in range(len(spisok)):
+          #print(f'Итерация{min}')
+          if min==spisok[i]:
+               print(i,spisok[i],min)
+               count=count+1
+               intervalmin=spisok[i]
+               print(count)
+               break
+          else:
+               continue
+     
+          # else:
+          #      print(f'В интервале отсуствует {min}')
+
+# for(min) in range(len(spisok)):
+#      if min==spisok[i]:
+#                print(i,spisok[i],min)
+#                count=count+1
+#                intervalmin=spisok[i]
+#                print(count)
+            
+
+
+
+       
+
+     
+   
+
+
+    
+
