4154e4dd0e113b1787862228be101b05ab045930
Pavel <black_yaguar@mail.ru>
2023-02-11 14:01:45 +0500

    Решение 22 задачи

--- Python_Home04/Task_22.py
+++ Python_Home04/Task_22.py
@@ -7,3 +7,26 @@
 # 2 4 6 8 10 12 10 8 6 4 2
 # 3 6 9 12 15 18
 # 6 12
+from random import randint
+first_list_element=int(input('Кол-во элементов первого списка='))
+first_list=[]
+for i in range(first_list_element):
+    first_list.append(randint(0,12))
+print(first_list)
+second_list_element=int(input('Кол-во элементов второго списка='))
+second_list=[]
+for i in range(second_list_element):
+    second_list.append(randint(0,12))
+print(second_list)
+first_list=set(first_list)
+second_list=set(second_list)
+united_list=first_list.intersection(second_list)
+united_list=list(united_list)
+united_list.sort()
+print(*united_list, "числа в обоих списках ")
+
+
+
+
+
+
