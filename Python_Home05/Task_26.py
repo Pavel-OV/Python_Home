c0c2fe8226041da6692c39e54e75acdc6a253934
Pavel <black_yaguar@mail.ru>
2023-02-14 12:32:52 +0500

    Решение задачи №26

--- Python_Home05/Task_26.py
+++ Python_Home05/Task_26.py
@@ -3,5 +3,13 @@
 # помощью рекурсии.
 # A = 3; B = 5 -> 243 (3⁵)
 # A = 2; B = 3 -> 8
+
+def calculate_degree(numberA,numberB):
+    if numberB==1:
+        return numberA
+    if numberB!=1:
+        return numberA*(calculate_degree(numberA,numberB-1))
 numberA=int(input('Введите число А='))
 numberB=int(input('Введите число В='))
+print(f'Результат возведения числа {numberA} в степень {numberB} равен', calculate_degree(numberA,numberB))
+ 
