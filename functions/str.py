d0a8e3ac0475a7c1b6e1db97019a36dd933389f5
Pavel <black_yaguar@mail.ru>
2023-02-19 13:21:25 +0500

    Папка для создания библитотеки

---
+++ functions/str.py
@@ -0,0 +1,6 @@
+# str_nums = ["4", "8", "6", "5", "3", "2", "8", "9", "2", "5"]
+# int_nums=list(map(int,str_nums))
+# print(str_nums)
+# print(int_nums)
+# string_it = ["processing", "strings", "with", "map"]
+# print(list(map(str.capitalize, string_it)))
