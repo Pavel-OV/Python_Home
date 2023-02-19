acf4d31da8a5c1abded18d59c07ad3a49eaa67d3
Pavel <black_yaguar@mail.ru>
2023-02-19 13:18:31 +0500

    Решение дополнительной задачи

--- Python_Home06/hard_soft.py
+++ Python_Home06/hard_soft.py
@@ -1,4 +1,5 @@
-# Задайте двумерный массив из целых чисел. Количество строк и столбцов задается с клавиатуры. Отсортировать элементы по возрастанию слева направо и сверху вниз.
+# Задайте двумерный массив из целых чисел. Количество строк и столбцов задается с клавиатуры.
+# Отсортировать элементы по возрастанию слева направо и сверху вниз.
 
 # Например, задан массив:
 # 1 4 7 2
@@ -8,3 +9,20 @@
 # 1 2 3 4
 # 5 7 9 10
 from random import randint
+rows=int(input('Колличество строк ='))
+column=int(input('Колличество  столбцов ='))
+mas = [] 
+for i in range(rows): 
+    for j in range(column):
+        mas.append(randint(0,10) )
+for i in range(rows):
+    for j in range(column):
+        print(mas[rows * j + i], end=" ")
+    print()
+print()
+mas.sort()
+for i in range(rows):
+    for j in range(column):
+        print(mas[column * i + j], end=" ")
+    print()
+
